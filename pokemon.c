717348ee4f3f15ae4336bd97684639f4768d6614
tomas-20 <tacuna20@stuy.edu>
2021-10-14 20:57:34 -0400

    made it so that whos_that_pokemon has a newline at the end

--- pokemon.c
+++ pokemon.c
@@ -5,7 +5,7 @@ struct pokemon {
   char *type;
 };
 void whos_that_pokemon(struct pokemon *p) {
-  printf("iiiiits %s, a %s-type pokemon!", p->name, p->type);
+  printf("iiiiits %s, a %s-type pokemon!\n", p->name, p->type);
 }
 struct pokemon *make_pokemon(char *name, char *type) {
   struct pokemon *output = malloc(16);
