636fb39409af4e7016651c452e196c7c0666b592
tomas-20 <tacuna20@stuy.edu>
2021-10-14 20:55:41 -0400

    created a make_pokemon function

--- pokemon.c
+++ pokemon.c
@@ -2,7 +2,13 @@
 struct pokemon {
   char *name;
   char *type;
-}
-void *whos_that_pokemon(struct pokemon *p) {
+};
+void whos_that_pokemon(struct pokemon *p) {
   printf("iiiiits %s, a %s-type pokemon!", p->name, p->type);
+}
+struct pokemon *make_pokemon(char *name, char *type) {
+  struct pokemon *output = malloc(16);
+  output->name = name;
+  output->type = type;
+  return output;
 }
